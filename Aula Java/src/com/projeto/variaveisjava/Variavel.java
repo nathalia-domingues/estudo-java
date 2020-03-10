9d5fd914476bb60ba623af2341a64e93944e6c6a
Julio Longo <julio.andrade321@gmail.com>
2020-03-10 00:10:21 -0300

    criado

---
+++ Aula Java/src/com/projeto/variaveisjava/Variavel.java
@@ -0,0 +1,25 @@
+package com.projeto.variaveisjava;
+
+public class Variavel {
+
+	public static void main(String[] args) {
+		
+
+		byte variavelByte = 10;
+		int variavelInt = 10000;
+		long variavelLong = 1000000;
+		float variavelFloat = 1.2f;
+		double variavelDouble = 1.232323;
+		boolean variavelBoolean = true;
+		String variavelString = "Nathália";
+		
+		System.out.println(variavelByte);
+		System.out.println(variavelInt);
+		System.out.println(variavelLong);
+		System.out.println(variavelFloat);
+		System.out.println(variavelDouble);
+		System.out.println(variavelBoolean);
+		System.out.println(variavelString + variavelByte);
+	}
+
+}
